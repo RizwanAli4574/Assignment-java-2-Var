8b84f610cf8399d932ca5b8ffcbd7f44132c1403
RizwanAli4574 <137984475+RizwanAli4574@users.noreply.github.com>
2023-09-25 12:34:12 +0500

    Assignment-java-2-Var

---
+++ apps.js
@@ -0,0 +1,24 @@
+var username = ("Rizwan Ali 116628");
+ console.log(username);
+
+var myname = ("Rizwan Ali");
+console.log  (myname)
+
+var message = ("Hello World");
+alert (message)
+
+var studentBio = ("My name is Rizwan Ali i am student in SMIT ")
+alert (studentBio)
+
+var pizza = ("pizza\npizz\npiz\npi\np"); 
+alert (pizza)
+
+var email = ("example@info.com");
+alert (email)
+
+
+var book = ("I am trying to learn from the book A smarter \n way to learn javascript");
+alert (book)
+
+var des = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+alert (des)
